c0b52e57c21e9d8c6d0648fbe8b07c416bf47319
Putut Yusri Bahtiar <64458989+pututyb@users.noreply.github.com>
2023-03-11 16:56:31 +0700

    Initial Commit

---
+++ CatatPenjualan./CatatPenjualan_App.swift
@@ -0,0 +1,17 @@
+//
+//  CatatPenjualan_App.swift
+//  CatatPenjualan.
+//
+//  Created by Putut Yusri Bahtiar on 11/03/23.
+//
+
+import SwiftUI
+
+@main
+struct CatatPenjualan_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
