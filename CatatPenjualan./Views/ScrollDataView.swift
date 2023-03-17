01df1a8887136d1a04235ca1f39f5bd7c0820886
Putut Yusri Bahtiar <64458989+pututyb@users.noreply.github.com>
2023-03-17 22:55:27 +0700

    implement search with NSPredicate

--- CatatPenjualan./Views/ScrollDataView.swift
+++ CatatPenjualan./Views/ScrollDataView.swift
@@ -13,31 +13,43 @@ struct ScrollDataView: View {
         SortDescriptor(\.nama),
         SortDescriptor(\.jumlah),
         SortDescriptor(\.harga)
-    ]) var catatan: FetchedResults<Catatan>
+    ], predicate: NSPredicate(value: true)) var catatan: FetchedResults<Catatan>
     
+    @State private var searchText = ""
+    @State private var predicate: NSPredicate = NSPredicate(value: true)
     
     let itemData = ["Bakso", "Otak-Otak", "Sosis", "Fishcake", "Sotong", "Sempol"]
     var body: some View {
-        List {
-            ForEach(catatan, id: \.self) { item in
-                HStack {
-                    Text(item.nama ?? "Uknown Item")
-                        .font(.system(size: 14, weight: .medium))
-                    Spacer()
-                    VStack(alignment: .leading) {
-                        Text("\(item.jumlah) Pcs")
-                            .font(.system(size: 14, weight: .medium))
-                        
-                        Text("Rp.\(item.harga)")
-                            .font(.system(size: 12, weight: .medium))
-                            .foregroundColor(item.harga > 0 ? .green : .red)
-                    }
+        VStack {
+            TextField("Cari Produk", text: $searchText) {
+                if !searchText.isEmpty {
+                    predicate = NSPredicate(format: "nama CONTAINS[cd] %@", searchText)
+                } else {
+                    predicate = NSPredicate(value: true)
                 }
-                .padding()
             }
-            .onDelete(perform: deleteCatatan)
-        }
-        .preferredColorScheme(.light)
+                .padding()
+                .background(.white)
+            List {
+                ForEach(catatan.filter { predicate.evaluate(with: $0)}, id: \.self) { item in
+                    HStack {
+                        Text(item.nama ?? "Uknown Item")
+                            .font(.system(size: 14, weight: .medium))
+                        Spacer()
+                        VStack(alignment: .leading) {
+                            Text("\(item.jumlah) Pcs")
+                                .font(.system(size: 14, weight: .medium))
+                            
+                            Text("Rp.\(item.harga)")
+                                .font(.system(size: 12, weight: .medium))
+                                .foregroundColor(item.harga > 0 ? .green : .red)
+                        }
+                    }
+                    .padding()
+                }
+                .onDelete(perform: deleteCatatan)
+            }
+        }.preferredColorScheme(.light)
     }
     
     func deleteCatatan(at offsets: IndexSet) {
